d4aff021ac7b896e3fb38408a442652016269846
Lee HuiGwang <104819764+winteryy@users.noreply.github.com>
2024-03-19 11:48:25 +0900

    design: Design Lv1 Ui

--- app/src/main/java/com/winterry/nbcsignin/SignInActivity.kt
+++ app/src/main/java/com/winterry/nbcsignin/SignInActivity.kt
@@ -3,9 +3,9 @@ package com.winterry.nbcsignin
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class SignInActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_sign_in)
     }
 }
