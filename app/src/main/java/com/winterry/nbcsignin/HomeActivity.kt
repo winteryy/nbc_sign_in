0f976664058af91405bb52df58d679c8b1e52a33
Lee HuiGwang <104819764+winteryy@users.noreply.github.com>
2024-03-19 17:00:48 +0900

    feat: Implemnet Lv5 features
    이미지 관련 코드들도 AppCompat 이용하도록 수정, 구버전 API svg 호환성 대비

--- app/src/main/java/com/winterry/nbcsignin/HomeActivity.kt
+++ app/src/main/java/com/winterry/nbcsignin/HomeActivity.kt
@@ -1,15 +1,24 @@
 package com.winterry.nbcsignin
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.content.res.AppCompatResources
+import androidx.appcompat.widget.AppCompatImageView
+import kotlin.random.Random
 
 class HomeActivity : AppCompatActivity() {
+
+    private val imageList = listOf(R.drawable.g80, R.drawable.g80_front, R.drawable.g80_inside, R.drawable.g80_inside_2, R.drawable.g80_sport)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        findViewById<AppCompatImageView>(R.id.userImageView).let { view ->
+            view.setImageDrawable(AppCompatResources.getDrawable(view.context, imageList[Random.nextInt(5)]))
+        }
 
         findViewById<TextView>(R.id.idTextView).text = try {
             "아이디: ${intent.getStringExtra(SignInActivity.ID)}"
