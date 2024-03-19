68c5d0701518676e93ce6df7e9b8b4835ccb6a3f
Lee HuiGwang <104819764+winteryy@users.noreply.github.com>
2024-03-19 16:14:45 +0900

    feat: Implement Lv3 features
    - Design Lv3 Ui, too

--- app/src/main/java/com/winterry/nbcsignin/HomeActivity.kt
+++ app/src/main/java/com/winterry/nbcsignin/HomeActivity.kt
@@ -2,10 +2,25 @@ package com.winterry.nbcsignin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class HomeActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        findViewById<TextView>(R.id.idTextView).text = try {
+            "아이디: ${intent.getStringExtra(SignInActivity.ID)}"
+        } catch (e: Exception) {
+            "아이디: None"
+        }
+
+        findViewById<Button>(R.id.finishButton).setOnClickListener {
+            finish()
+        }
+
     }
+
 }
